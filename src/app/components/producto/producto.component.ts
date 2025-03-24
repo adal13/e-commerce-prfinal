5bbb5f3b4d71955b04e731f4852e9f8134c54163
Adal, Rommel, Elsa <adal13gomez.agg@gmail.com>
2025-03-23 19:47:26 -0600

    Maquetacion de la vista producto, mostrando informacion de manera estatica

---
+++ src/app/components/producto/producto.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Producto, ProductoService } from 'src/app/service/producto.service';
+
+@Component({
+  selector: 'app-producto',
+  templateUrl: './producto.component.html',
+  styleUrls: ['./producto.component.css']
+})
+
+export class ProductoComponent {
+
+  productos: Producto[] = [];
+  productoForm: FormGroup;
+  editIndex: number | null = null;
+
+  constructor(private productoService: ProductoService, private fb: FormBuilder) {
+    this.productoForm = this.fb.group({
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      precio: [0, [Validators.required, Validators.min(0)]],
+      stock: [0, [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.productos = this.productoService.getProductos();
+  }
+
+  guardarProducto() {
+    if (this.productoForm.valid) {
+      const productoData: Producto = { id: 0, ...this.productoForm.value };
+
+      if (this.editIndex !== null) {
+        this.productoService.editarProducto(this.editIndex, productoData);
+        this.editIndex = null;
+      } else {
+        this.productoService.agregarProducto(productoData);
+      }
+
+      this.productoForm.reset({ precio: 0, stock: 0 });
+      (document.getElementById('cerrarModal') as HTMLButtonElement).click();
+      this.productos = this.productoService.getProductos();
+    }
+  }
+
+  editarProducto(index: number) {
+    this.editIndex = index;
+    const producto = this.productos[index];
+    this.productoForm.patchValue(producto);
+  }
+
+  eliminarProducto(index: number) {
+    if (confirm('¿Seguro que deseas eliminar este producto?')) {
+      this.productoService.eliminarProducto(index);
+      this.productos = this.productoService.getProductos();
+    }
+  }
+
+}
