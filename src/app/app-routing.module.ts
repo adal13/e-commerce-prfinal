72ed9d9983ff8f34f9b634829806cad1758cead7
Adal, Rommel, Elsa <adal13gomez.agg@gmail.com>
2025-03-23 19:48:06 -0600

    Manteniendo la logica de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { PedidoComponent } from './components/pedido/pedido.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  { path: 'inicio', component: InicioComponent },
+  { path: 'clientes', component: ClienteComponent },
+  { path: 'pedidos', component: PedidoComponent },
+  { path: 'productos', component: ProductoComponent },
+  { path: '**', redirectTo: 'inicio' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
