4afb1959aaa81b13368e9ec97e54c6637d52d6d3
Adal, Rommel, Elsa <adal13gomez.agg@gmail.com>
2025-03-23 19:50:12 -0600

    Agregando importaciones al modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { PedidoComponent } from './components/pedido/pedido.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ClienteComponent,
+    PedidoComponent,
+    ProductoComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
